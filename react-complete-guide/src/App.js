010287f0829ff2853f63643074c4e25a800f7b3a
Luiz Flavio Costa Lima <luiz.lima@dtidigital.com.br>
2020-03-04 14:48:26 -0300

    First Commit react-complete-guide

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // RETURN (
+  //   <DIV CLASSNAME="APP">
+  //     <H1>HI, I'M A A REACT APP</H1>
+  //   </DIV>
+  // );
+  // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
+  return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
 }
 
 export default App;
