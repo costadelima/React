d2cd525ecf83dbdb83e8d9caa51c70a290aae9e9
Luiz Flavio Costa Lima <luiz.lima@dtidigital.com.br>
2020-04-27 10:59:53 -0300

    s3ep37 - Understanding the children Prop

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a react app</h1>
-  </div>
+      <p>This is really working!</p>
+      <Person name="Luiz" age="28"/>
+      <Person name="Manu" age="29">My Hobbies: Racing</Person>
+      <Person name="Stephanie" age="26"/>
+    </div>
   );
 }
 
