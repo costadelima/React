a5d4bc8808f541ebf2677922e8a7a313e6ee6180
Luiz Flavio Costa Lima <luiz.lima@dtidigital.com.br>
2020-07-08 15:31:30 -0300

    code smell

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,7 +9,6 @@ import React from 'react';
 // import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
 
 class App extends Component {
   /**
